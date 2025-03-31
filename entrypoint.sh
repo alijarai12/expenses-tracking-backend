f5ef6ec3ab1841f41a4e3659395ea15c91b9d8fa
alija <alijarai414@gmail.com>
2025-03-31 12:02:05 +0545

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Wait for the DB to be available before proceeding
-/myapp/wait-for-it.sh db:5432 --timeout=30 --strict -- echo "Postgres is up"
-
 # Apply migrations
 python manage.py migrate
 
